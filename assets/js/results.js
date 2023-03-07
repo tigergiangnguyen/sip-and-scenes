ce0498622ba9d816a0cca80950bdb17375ebff8f
Gabby <gabbympinto@gmail.com>
2023-03-06 20:11:25 -0500

    finished cocktailAPI functions

--- assets/js/results.js
+++ assets/js/results.js
@@ -20,15 +20,44 @@ getDrink().then(function() {
 // Renders drinks as HTML elements and displays them to the page
 function renderDrinks() {
     console.log(drinkArray);
-    var drinkName = $('.drinkName');
-    var drinkDesc = $('.drinkDesc');
-    var drinkImg = $('.drinkImg');
-    var currentDrink = 0;
 
-    $.each(drinkArray, function(i) {
-       console.log(i)
-       drinkName[i].text = drinkArray[currentDrink];
-    });
+    for (i = 0; i < drinkArray.length; i++) {
+        var currentDrink = drinkArray[i];
+        console.log(currentDrink);
+
+        // 1. create div with class of drinks
+        var drinkDiv = $('<div>').addClass("drinks");
+
+        // 2. create h3 with class of drinkName
+        // - text is strDrink
+        // - append to div class drinks
+        var drinkName = $('<h3>').addClass("drinkName");
+        drinkName.text(currentDrink.strDrink);
+        drinkDiv.append(drinkName);
+
+
+        // 3. create a p tag with class drinkIns
+        // - text is strInstructions
+        // - append to div class drinks
+        var drinkIns = $('<p>').addClass("drinkIns");
+        drinkIns.text(currentDrink.strInstructions);
+        drinkDiv.append(drinkIns);
+
+        // 4. create img with class of drinkImg
+        // - set image src attribute to strDrinkThumb
+        // - append to div class drinks
+        var drinkImg = $('<img>').addClass("drinkImg").attr("src", currentDrink.strDrinkThumb);
+        drinkDiv.append(drinkImg);
+
+        // 5. append to cocktail container
+        $('#cocktail-container').append(drinkDiv);
+        
+    }
    
 }
 
+{/* <div id="drink-1" class="drinks">
+            <h3 class="drinkName"></h3>
+            <p class="drinkDesc"></p>
+            <img class="drinkImg">
+        </div> */}
