a4d389310b127a5a208b5fe9e23418c3a9d4c6d0
kathrynstack <kathrynstack17@gmail.com>
2023-03-07 14:41:40 -0500

    css additions, condition to prevent reaching api call limit, code for drink ingredients

--- assets/js/results.js
+++ assets/js/results.js
@@ -35,10 +35,23 @@ return fetch ('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 };
 
 
-// Copies getDrink to get second drink
-getDrink().then(function() {
-    getDrink().then(renderDrinks);
-});
+//Used for testing so we dont spam the api and reach the limit
+var access_api = true;
+if (access_api) 
+{
+    // Copies getDrink to get second drink
+    getDrink().then(function () {
+        getDrink().then(renderDrinks);
+    });
+}
+else 
+{
+    //For testing purposes, show drink without accessing api
+    var data = JSON.parse('{"drinks":[{"idDrink":"11102","strDrink":"Black Russian","strDrinkAlternate":null,"strTags":"IBA,ContemporaryClassic","strVideo":null,"strCategory":"Ordinary Drink","strIBA":"Contemporary Classics","strAlcoholic":"Alcoholic","strGlass":"Old-fashioned glass","strInstructions":"Pour the ingredients into an old fashioned glass filled with ice cubes. Stir gently.","strInstructionsES":null,"strInstructionsDE":"Die Zutaten in ein old fashioned Glas mit Eiswürfeln geben. Vorsichtig umrühren.","strInstructionsFR":null,"strInstructionsIT":"Versare gli ingredienti in un bicchiere vecchio stile pieno di cubetti di ghiaccio. Mescola delicatamente.","strInstructionsZH-HANS":null,"strInstructionsZH-HANT":null,"strDrinkThumb":"https://www.thecocktaildb.com/images/media/drink/8oxlqf1606772765.jpg","strIngredient1":"Coffee liqueur","strIngredient2":"Vodka","strIngredient3":null,"strIngredient4":null,"strIngredient5":null,"strIngredient6":null,"strIngredient7":null,"strIngredient8":null,"strIngredient9":null,"strIngredient10":null,"strIngredient11":null,"strIngredient12":null,"strIngredient13":null,"strIngredient14":null,"strIngredient15":null,"strMeasure1":"3/4 oz ","strMeasure2":"1 1/2 oz ","strMeasure3":null,"strMeasure4":null,"strMeasure5":null,"strMeasure6":null,"strMeasure7":null,"strMeasure8":null,"strMeasure9":null,"strMeasure10":null,"strMeasure11":null,"strMeasure12":null,"strMeasure13":null,"strMeasure14":null,"strMeasure15":null,"strImageSource":"https://commons.wikimedia.org/wiki/File:Black_Russian.jpg","strImageAttribution":"firstName lastName","strCreativeCommonsConfirmed":"Yes","dateModified":"2017-09-02 16:54:49"}]}');
+    drinkArray.push(data.drinks[0]);
+    renderDrinks();
+}
+
 
 // Renders drinks as HTML elements and displays them to the page
 function renderDrinks() {
@@ -51,6 +64,22 @@ function renderDrinks() {
         var drinkName = $('<h3>').addClass("drinkName");
         drinkName.text(currentDrink.strDrink);
         drinkDiv.append(drinkName);
+
+        // created a ul with the class drinkIngredients
+        var ingredientList = $('<ul>').addClass("drinkIngredients");
+        //for loop to go over all the ingredients and measurements (the api returns 15 by default)
+        for (var j = 0; j < 15; j++) {
+            //if statement to check if ingredient and measurement are defined
+            if (currentDrink['strIngredient' + j] && currentDrink['strMeasure' + j]) {
+                // created a li with the class drinkIngredient
+                var ingredient = $('<li>').addClass("drinkIngredient");
+                //sets the text of the ingredient and the measurement separated by a space
+                ingredient.text(currentDrink['strIngredient' + j] + ' - ' + currentDrink['strMeasure' + j]);
+                ingredientList.append(ingredient);
+            }
+        }
+        drinkDiv.append(ingredientList);
+
         // p element to hold drink instructions
         var drinkIns = $('<p>').addClass("drinkIns");
         drinkIns.text(currentDrink.strInstructions);
